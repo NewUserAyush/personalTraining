b4aabcbd92b2ea187c9e3fc01df99a6c18a86b10
Admin <ayushjswl3@gmail.com>
2016-02-19 14:30:59 +0530

    initail version 1.7

---
+++ source/src/app/pt/Admin/Instructor/instructor.controller.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pt.Admin')
+        .controller('InstructorController', fnCtrl);
+
+    /* @ngInject */
+    function fnCtrl(instructorService,$state) {
+        var vm = this;
+
+      vm.items=instructorService.getData();
+      vm.getDetail = getDetail;
+
+        function getDetail(test) {
+            $state.go("triangular-no-scroll.admin-default-no-scroll.instructor.insDetails", {
+                id: test.id
+            });
+
+        }
+    }
+})();
