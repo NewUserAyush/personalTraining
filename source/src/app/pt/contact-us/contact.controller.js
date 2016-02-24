85faba720137bf8ebac6754f729757c097435fb4
ayushjswl3@gmail.com <ayush513>
2016-02-24 15:44:20 +0530

    updated form

--- source/src/app/pt/contact-us/contact.controller.js
+++ source/src/app/pt/contact-us/contact.controller.js
@@ -6,7 +6,7 @@
         .controller('contactController', fn);
 
     /* @ngInject */
-    function fn(contactmodel,$scope) {
+    function fn(contactmodel,$scope,$timeout, $mdToast,instructormodel) {
         var vm = this;
 
         //vm.testData = ['contact', '#is', 'great'];
@@ -24,7 +24,51 @@
             return day === 0 || day === 6;
         }
 
+        //image upload
+
+        var vm = this;
+        vm.status = 'idle';  // idle | uploading | complete
+        vm.upload = upload;
+
+        vm.items=instructormodel.getData()
+        var fileList;
+
+        function upload($files) {
+            if($files !== null && $files.length > 0) {
+                fileList = $files;
+
+                uploadStarted();
+
+                $timeout(uploadComplete, 4000);
+            }
+        }
+
+        function uploadStarted() {
+            vm.status = 'uploading';
+        }
+
+        function uploadComplete() {
+            vm.status = 'complete';
+            var message = 'Thanks for ';
+            for(var file in fileList) {
+                message += fileList[file].name + ' ';
+            }
+            $mdToast.show({
+                template: '<md-toast><span flex>' + message + '</span></md-toast>',
+                position: 'bottom right',
+                hideDelay: 5000
+            });
+
+            $timeout(uploadReset, 3000);
+        }
+
+        function uploadReset() {
+            vm.status = 'idle';
+        }
+
+
         vm.testData=contactmodel.getData();
+
 
        vm.myfunc =function myfun(studentInfo)
        {
